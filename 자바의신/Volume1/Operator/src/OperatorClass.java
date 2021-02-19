ec0a1d5c4a7269af38489f17719d060e480eb81a
Chan <ieejo716@naver.com>
2021-02-20 01:39:34 +0900

    README

--- 자바의신/Volume1/Operator/src/OperatorClass.java
+++ 자바의신/Volume1/Operator/src/OperatorClass.java
@@ -1,9 +1,11 @@
 import java.util.*;
 public class OperatorClass {
     public static void main(String[] args) {
-        int value = 1;
-        System.out.println(value++);
-        System.out.println(value);
-        System.out.println(++value);
+        byte a = 127;
+        byte b = 1;
+
+        System.out.println(~a);
+        System.out.println(~b);
+
     }
 }

4a0f4b52a6b9bb373068ce232be3d24d0f2df27c
Chan <ieejo716@naver.com>
2021-03-15 22:03:00 +0900

    Annotation

---
+++ 자바의신/Volume1/Annotation/src/Annotation.java
@@ -0,0 +1,8 @@
+public class Annotation extends Parent{
+
+    public static void main(String[] args) {
+        Annotation annotation = new Annotation();
+        annotation.printName();
+        annotation.noMoreUse();
+    }
+}

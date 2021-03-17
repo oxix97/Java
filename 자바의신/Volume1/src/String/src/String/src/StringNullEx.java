1ac18ef6d4b4687d77cc73949ee3dc9c0ff09c3e
Chan <ieejo716@naver.com>
2021-03-17 17:51:53 +0900

    commit message

---
+++ 자바의신/Volume1/src/String/src/String/src/StringNullEx.java
@@ -0,0 +1,17 @@
+package String.src;
+
+public class StringNullEx {
+    public boolean nullCheck(String data) {
+
+        if (data == null)
+            return true;
+
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        StringNullEx ex  = new StringNullEx();
+        ex.nullCheck(null);
+    }
+}
